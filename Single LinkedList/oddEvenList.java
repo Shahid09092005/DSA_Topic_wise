26bf038a16306af0b0cf8a252312b4a4e0cbc441
Shahid Mansuri <140784065+Shahid09092005@users.noreply.github.com>
2025-03-24 01:34:35 +0530

    Medium

---
+++ Single LinkedList/oddEvenList.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if(head==null||head.next==null) return head;
+        ListNode o=head;
+        ListNode e=head.next;
+        ListNode c=e;
+        while(e!=null && e.next!=null){
+                o.next=o.next.next;
+                o=o.next;
+                e.next=e.next.next;
+                e=e.next;
+        }
+        o.next=c;
+        return head;
+        
+    }
+}
