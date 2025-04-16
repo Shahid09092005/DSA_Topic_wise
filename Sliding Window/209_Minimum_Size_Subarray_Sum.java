38f8b30f0644b8aeef75dbd3d83c83990069a36d
Shahid Mansuri <140784065+Shahid09092005@users.noreply.github.com>
2025-04-16 21:13:52 +0530

    M

---
+++ Sliding Window/209_Minimum_Size_Subarray_Sum.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int j=0;
+        int currSum = 0;
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<nums.length;i++){
+            currSum  +=nums[i];
+            while(currSum>=target){
+                min = Math.min(min,i-j+1);
+                currSum -=nums[j];
+                j++;
+            }  
+        }
+        if(min==Integer.MAX_VALUE) return 0;
+        return min;
+    }
+}
